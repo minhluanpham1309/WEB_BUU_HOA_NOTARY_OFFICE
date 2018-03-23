41709a9f0c0b15b341055c0a1d52c12e232bfc78
Thy Hour <hourthy88@gmail.com>
2018-03-23 15:11:38 +0700

    Show Account Information

--- VanPhongCongChung/VanPhongCongChung/Controllers/Admin/AdminController.cs
+++ VanPhongCongChung/VanPhongCongChung/Controllers/Admin/AdminController.cs
@@ -16,6 +16,27 @@ namespace VanPhongCongChung.Controllers
         {
             return View();
         }
+
+        public ActionResult AdminAccount()
+        {
+            NotaryOfficeDB db = new NotaryOfficeDB();
+            List<Account> accounts = db.Accounts.ToList();
+            List<Account> showAccount = accounts.Select(x => new Account
+            {
+                ID_Account = x.ID_Account,
+                UserName = x.UserName,
+                Passwords = x.Passwords,
+                ID_Account_Type = x.ID_Account_Type,
+                Note = x.Note,
+                AccountID = x.AccountID,
+                Create_Account = x.Create_Account,
+                Update_Account = x.Update_Account,
+                Is_Delete = x.Is_Delete,
+                Is_Not_Delete = x.Is_Not_Delete,
+                Folder_Path = x.Folder_Path
+            }).ToList();
+            return View(showAccount);
+        }
         
     }
 }
